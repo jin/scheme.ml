0ce8831e245a28a0615ab4ee449ff408e549900c
Jingwen Chen <jin@crypt.sg>
2016-04-18 19:32:08 +0800

    token -> s-exp done

--- src/lexer.ml
+++ src/lexer.ml
@@ -10,24 +10,14 @@ let letter = [%sedlex.regexp? 'a'..'z' | 'A'..'Z']
 let boolean = [%sedlex.regexp? "#t" | "#f" ]
 let digit = [%sedlex.regexp? '0'..'9']
 let number = [%sedlex.regexp? Plus digit]
-
-let atom = [%sedlex.regexp? letter, Star letter]
-
-let p s b = Printf.printf s (Sedlexing.Utf8.lexeme b);;
+let ident = [%sedlex.regexp? letter, Star letter]
 
 let string_of_buf (buf: Sedlexing.lexbuf) = Sedlexing.Utf8.lexeme buf
 
-let number_of_buf (buf: Sedlexing.lexbuf) = int_of_string (string_of_buf buf)
-
-let boolean_of_buf (buf: Sedlexing.lexbuf) = match string_of_buf buf with
-  | "#t" -> true 
-  | "#f" -> false
-  | _ -> failwith "Unable to parse boolean"
-
 type token = 
   Ident of string |
-  Number of int |
-  Boolean of bool |
+  Number of string |
+  Boolean of string |
   LParen |
   RParen |
   EOF
@@ -35,40 +25,64 @@ type token =
 let string_of_token token =
   match token with
   | Ident s -> s
-  | Number s -> "Number "^(string_of_int s)
-  | Boolean s -> if s then "Boolean #t" else "Boolean #f"
+  | Number s -> "Number "^s
+  | Boolean s -> "Boolean "^s 
   | LParen -> "LParen"
-  | RParen -> "LParen"
+  | RParen -> "RParen"
   | EOF -> "\n"
 
 type sexp = Atom of token | List of sexp list
 
 let string_of_tokens tokens = 
-  "["^(String.concat ", " (List.map (fun token -> string_of_token token) tokens))^"]"
+  "["^
+    (String.concat 
+       ", " 
+       (List.map (fun token -> string_of_token token) tokens)
+    )
+  ^"]"
+
+let rec string_of_sexp sexpr = 
+  match sexpr with 
+  | Atom x -> string_of_token x
+  | List xs -> "("^(String.concat " " (List.map string_of_sexp xs))^")"
 
 let rec tokenize buf tokens =
   match%sedlex buf with
   | white_space -> tokenize buf tokens
-  | number -> tokenize buf (tokens@[Number (number_of_buf buf)])
-  | boolean -> tokenize buf (tokens@[Boolean (boolean_of_buf buf)])
-  | atom -> tokenize buf (tokens@[Ident (string_of_buf buf)])
+  | number -> tokenize buf (tokens@[Number (string_of_buf buf)])
+  | boolean -> tokenize buf (tokens@[Boolean (string_of_buf buf)])
+  | ident -> tokenize buf (tokens@[Ident (string_of_buf buf)])
   | '(' ->  tokenize buf (tokens@[LParen])
   | ')' ->  tokenize buf (tokens@[RParen])
   | eof -> tokens 
   | _ -> failwith "Lexer error: Unexpected character"
 
-let to_sexp (tokens: token list) = 
+(* Convert a list of tokens into an s-expression *)
+let sexp_of_tokens (tokens: token list) = 
+  let rec sexp_of_list tokens sexpr =
+    match tokens with 
+    | [] -> (List sexpr, [])
+    | RParen::rem_tokens -> (List sexpr, rem_tokens) 
+    | LParen::rem_tokens -> 
+      let (nested_list_sexpr, rem_tokens) = sexp_of_list rem_tokens [] in
+      sexp_of_list rem_tokens (sexpr@[nested_list_sexpr])
+    | x::rem_tokens -> sexp_of_list rem_tokens (sexpr@[Atom x])
+  in
   let rec aux toks sexpr =
     match toks with 
     | [] -> sexpr
+    | LParen::rem_tokens -> 
+      let (list_sexpr, rem_tokens) = sexp_of_list rem_tokens [] in
+      aux rem_tokens (sexpr@[list_sexpr])
     | _ -> sexpr in
-  aux tokens []
+  let sexpr = aux tokens [] in List sexpr
 
 
 let () =
   let lexbuf = Sedlexing.Utf8.from_string ex_list_nested in
   let tokens = tokenize lexbuf [] in
   let _ = print_endline (string_of_tokens tokens) in
-  let sexpr = to_sexp tokens in
+  let sexpr = sexp_of_tokens tokens in
+  let _ = print_endline (string_of_sexp sexpr) in
   ()
 ;;
